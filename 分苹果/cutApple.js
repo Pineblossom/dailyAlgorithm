6a5d5a79e50e2dcdc4b717841459fcb94e893b1d
Pineblossom <2383788860@qq.com>
2020-05-09 14:49:54 +0800

    苹果分赃

---
+++ 分苹果/cutApple.js
@@ -0,0 +1,21 @@
+for (let i = 5; i < 999999; i++) {
+    if (i%5 === 1) {
+        let first = i-Math.floor(i/5)-1
+        if (first%5 === 1) {
+            let second = first - Math.floor(first/5) -1
+            if (second%5 === 1) {
+                let third = second - Math.floor(second/5) -1
+                if (third%5 === 1) {
+                    let fourth = third - Math.floor(third/5) - 1
+                    if (fourth%5 === 1) {
+                        let fifth = fourth - Math.floor(fourth/5) -1
+                        if (fifth%5 === 1) {
+                            console.log("最少有" + i + "个苹果")
+                            return i
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
